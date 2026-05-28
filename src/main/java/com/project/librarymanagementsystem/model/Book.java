7057007bbeedfed3d1951a53b256b1657492dbc7
ayush pundir <ayush.pundir@globallogic.com>
2026-05-28 15:02:03 +0530

    feat: implement Book, Borrower, and Loan Hibernate entities

---
+++ src/main/java/com/project/librarymanagementsystem/model/Book.java
@@ -0,0 +1,31 @@
+package com.project.librarymanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(unique = true, nullable = false)
+    private String isbn;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean isAvailable = true;
+}
