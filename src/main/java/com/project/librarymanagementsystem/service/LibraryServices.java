a21265602c6f3ad295dba62b53a9b570661da792
ayush pundir <ayush.pundir@globallogic.com>
2026-05-28 15:51:22 +0530

    feat: Cerated the services fot the application.

--- src/main/java/com/project/librarymanagementsystem/service/LibraryServices.java
+++ src/main/java/com/project/librarymanagementsystem/service/LibraryServices.java
@@ -1,10 +1,17 @@
 package com.project.librarymanagementsystem.service;
 
+import com.project.librarymanagementsystem.dto.BookDTO;
+import com.project.librarymanagementsystem.dto.BorrowerDTO;
+import com.project.librarymanagementsystem.dto.LoanResponseDTO;
 import com.project.librarymanagementsystem.model.Book;
 import com.project.librarymanagementsystem.model.Borrower;
 
 import java.util.List;
 
 public interface LibraryServices {
-
+    public List<BookDTO> getAllBooks();
+    public BookDTO addBook(BookDTO bookDTO);
+    public List<BorrowerDTO> getAllBorrowers();
+    public LoanResponseDTO checkOutBook(Long bookId, Long borrowerId);
+    public List<LoanResponseDTO> checkAndFlagOverdueLoans();
 }
